aa733892c9fb038d75be1ba8473851be292482e2
firezombie34 <42081928+firezombie34@users.noreply.github.com>
2020-10-24 22:27:42 -0500

    Add files via upload

--- src/main/java/com/coznix/catastrophes/events/ModClientEvents.java
+++ src/main/java/com/coznix/catastrophes/events/ModClientEvents.java
@@ -2,27 +2,24 @@ package com.coznix.catastrophes.events;
 
 import com.coznix.catastrophes.Catastrophes;
 import com.coznix.catastrophes.util.RegistryHandler;
-import net.minecraft.client.tutorial.Tutorial;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoader;
 import net.minecraftforge.fml.common.Mod;
-
 
 @Mod.EventBusSubscriber(modid = Catastrophes.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ModClientEvents {
 
-    @SubscribeEvent // LivingEntity#
-    public static void onJumpWithStick(LivingEvent.LivingJumpEvent event) {
+    @SubscribeEvent
+    public static void onJumpWithCatalyst(LivingEvent.LivingJumpEvent event) {
         LivingEntity player = event.getEntityLiving();
-        if (player.getHeldItemMainhand().getItem() == Items.STICK)  {
-            Catastrophes.LOGGER.info("Player tried to jump with a stick!");
-            World world = player.getEntityWorld();
-            world.setBlockState(player.func_233580_cy_().add(0, -1, 0), RegistryHandler.RUBY_BLOCK.get().getDefaultState());
-         }
-     }
+        if (player.getHeldItemMainhand().getItem() == RegistryHandler.CATALYST.get()) {
+        }
+    }
 }
