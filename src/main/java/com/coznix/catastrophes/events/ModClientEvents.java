65f250bfc420ba341a0020254a68da2112b6bf0a
firezombie34 <42081928+firezombie34@users.noreply.github.com>
2020-10-25 14:39:21 -0500

    Add files via upload

--- src/main/java/com/coznix/catastrophes/events/ModClientEvents.java
+++ src/main/java/com/coznix/catastrophes/events/ModClientEvents.java
@@ -1,9 +1,11 @@
 package com.coznix.catastrophes.events;
-
 import com.coznix.catastrophes.Catastrophes;
 import com.coznix.catastrophes.util.RegistryHandler;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
@@ -16,10 +18,19 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = Catastrophes.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ModClientEvents {
 
+
     @SubscribeEvent
-    public static void onJumpWithCatalyst(LivingEvent.LivingJumpEvent event) {
+    public static void onCatalystUseAir(PlayerInteractEvent.RightClickItem event) {
         LivingEntity player = event.getEntityLiving();
         if (player.getHeldItemMainhand().getItem() == RegistryHandler.CATALYST.get()) {
+            World world = player.getEntityWorld();
+
+            PlayerEntity player_for_text = event.getPlayer();
+
+            if (!event.getPlayer().getEntityWorld().isRemote) {
+                String msg = TextFormatting.DARK_RED + "The Catastrophe Cycle has been Enabled!";
+                player.sendMessage(new StringTextComponent(msg), player.getUniqueID());
+            }
         }
     }
 }
--- src/main/java/com/coznix/catastrophes/events/ModServerEvents.java
+++ src/main/java/com/coznix/catastrophes/events/ModServerEvents.java
@@ -1,15 +1,11 @@
 package com.coznix.catastrophes.events;
 
 import com.coznix.catastrophes.Catastrophes;
+import net.minecraft.world.storage.ServerWorldInfo;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = Catastrophes.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.DEDICATED_SERVER)
+@Mod.EventBusSubscriber(modid = Catastrophes.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ModServerEvents { }
 
-public class ModServerEvents {
-
-    @SubscribeEvent
-    public static void onServerChat(ServerChatEvent event) { }
-}
